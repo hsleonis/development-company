4ca817c377666c381f81d06cd719448cdf6c8361
Shahriar <hsleonis@gmail.com>
2016-02-22 12:06:29 +0600

    Dev tools started

---
+++ wp-content/plugins/wp-dev-tools/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: WP DEV TOOLS
+Plugin URI: http://github.com/hsleonis
+Description: Development tools for wordpress coders
+Version: 1.0
+Author: Md. Hasan Shahriar
+Author URI: http://themeaxe.com
+License: GPL-v3
+*/
+/*
+Copyright 2016  Md. Hasan Shahriar  (email : hsleonis2@gmail.com)
+
+This program is paid software; you can not redistribute it and/or modify
+it under the terms of the GNU General Public License, version 3, as 
+published by the Free Software Foundation.
+*/
