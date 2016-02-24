a74b977e6ca1a3876e4d35e03c3a345f2cda68e7
Tanmoy Biswas <bravotanmoy@hotmail.com>
2016-02-24 12:49:46 +0600

    Modified files

---
+++ wp-content/themes/arrow/gallery.php
@@ -0,0 +1,7 @@
+<?php /*get_header(); */?>
+
+<?php include("header.php"); ?>
+
+csc
+<?php include("footer.php"); ?>
+<?php /*get_footer(); */?>
--- wp-content/themes/arrow/single-projects.php
+++ wp-content/themes/arrow/single-projects.php
@@ -33,49 +33,54 @@
 	   			<!-- end of spec project cat -->
 
 	   			<!-- project detail -->
-	   			<div class="spec-project-detail">
+	   			<div class="spec-project-detail tab-content">
 	   				<div class="spec-project-nav">
 	   					<h3><?php the_title(); ?> at gulshan 2, dhaka</h3>
                         <?php previous_post_link( '%link', '<i class="sprite sprite-left-arrow"></i>', TRUE, ' ', 'status' ); ?>
                         <?php next_post_link( '%link', '<i class="sprite sprite-right-arrow"></i>', TRUE, ' ', 'status' ); ?>
 	   				</div>
-	   				<div class="spec-info">
+	   				<div id="a-glance" class="spec-info tab-pane fade in active">
 	   					<?php the_content(); ?>
+						<p>Tab 1</p>
 	   				</div>
+					<div id="specification" class="spec-info tab-pane fade">
+						<?php the_content(); ?>
+						<p>Tab 2</p>
+					</div>
 	   			</div>
 	   			<!-- end of project detail -->
 	   		</div>
 	   		<div class="col-md-7 col-sm-6 col-xs-5 remove-padding height100 spec-right-wrapper">
 	   			<div class="spec-project-tab">
-	   				<ul class="spec-project-tab-ul">
-	   					<li class="col-md-3 col-sm-12 col-xs-12 remove-padding">
-		   					<a href="gallery.php">
-		   						<i class="sprite sprite-camera display-block"></i>
-		   						<i class="sprite sprite-hover-camera display-none"></i>
-		   						<span>gallery</span>
-		   					</a>
-	   					</li>
-	   					<li class="col-md-3 col-sm-12 col-xs-12 remove-padding">
-		   					<a href="#">
-		   						<i class="sprite sprite-floor-map display-block"></i>
-		   						<i class="sprite sprite-hover-floor-map display-none"></i>
-		   						<span>floor plans</span>
-		   					</a>
-	   					</li>
-	   					<li class="col-md-3 col-sm-12 col-xs-12 remove-padding">
-		   					<a href="#">
-		   						<i class="sprite sprite-spec display-block"></i>
-		   						<i class="sprite sprite-hover-spec display-none"></i>
-		   						<span>specification</span>
-		   					</a>
-	   					</li>
-	   					<li class="col-md-3 col-sm-12 col-xs-12 remove-padding">
-		   					<a href="#">
-		   						<i class="sprite sprite-at-a-glance display-block"></i>
-		   						<i class="sprite sprite-hover-at-a-glance display-none"></i>
-		   						<span>at a glance</span>
-		   					</a>
-	   					</li>
+	   				<ul class="spec-project-tab-ul nav nav-tabs">
+						<li class="col-md-3 col-sm-12 col-xs-12 remove-padding active">
+							<a href="#a-glance" data-toggle="tab">
+								<i class="sprite sprite-at-a-glance display-block"></i>
+								<i class="sprite sprite-hover-at-a-glance display-none"></i>
+								<span>at a glance</span>
+							</a>
+						</li>
+						<li class="col-md-3 col-sm-12 col-xs-12 remove-padding">
+							<a href="#specification" data-toggle="tab">
+								<i class="sprite sprite-spec display-block"></i>
+								<i class="sprite sprite-hover-spec display-none"></i>
+								<span>specification</span>
+							</a>
+						</li>
+						<li class="col-md-3 col-sm-12 col-xs-12 remove-padding">
+							<a href="#">
+								<i class="sprite sprite-floor-map display-block"></i>
+								<i class="sprite sprite-hover-floor-map display-none"></i>
+								<span>floor plans</span>
+							</a>
+						</li>
+						<li class="col-md-3 col-sm-12 col-xs-12 remove-padding">
+							<a href="#">
+								<i class="sprite sprite-camera display-block"></i>
+								<i class="sprite sprite-hover-camera display-none"></i>
+								<span>gallery</span>
+							</a>
+						</li>
 	   				</ul>
 	   			</div>
 	   		</div>
